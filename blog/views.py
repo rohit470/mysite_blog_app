d1db2d138f17b9c81b1276ba303da10fe7dfa97c
Rohit Tripathi <rohittripathi200@gmail.com>
2025-06-25 11:25:35 +0530

    template initial changes

--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("<h1>blog_app</h1>")
+    return render(request,'blog/home.html')
 
 def about(request):
-    return HttpResponse("<h1>about_app</h1>")
+    return render(request,'blog/about.html')
