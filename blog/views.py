ba8b5cb83e330aad29c235217be0abc1e10cf23d
Rohit Tripathi <rohittripathi200@gmail.com>
2025-07-01 13:01:05 +0530

    dummy post data

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+    {
+        'author':'Rohit',
+        'title':'Blog Post 1',
+        'content':'First post content',
+        'date_posted':'July 1,2025',
+    },
+    {
+        'author':'Rahul',
+        'title':'Blog Post 2',
+        'content':'Second post content',
+        'date_posted':'July 2,2025',
+    }
+]
+
 # Create your views here.
 def home(request):
-    return render(request,'blog/home.html')
+    context={'posts':posts}
+    return render(request,'blog/home.html',context)
 
 def about(request):
     return render(request,'blog/about.html')
